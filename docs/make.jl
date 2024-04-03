93137ccc4d11043b9711f0ed4451b943d02175f2
Scott McIntire <scottrsm@gmail.com>
2024-04-03 16:40:12 -0400

    Added github doc and test actions

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,18 @@
-using Documenter, Wordle
+using Wordle
+import Pkg
 
-makedocs(sitename="Wordle Docs")
+Pkg.add("Documenter")
+using Documenter
 
+makedocs(
+	sitename = "Wordle",
+	format = Documenter.HTML(),
+	modules = [Wordle]
+	)
+
+	# Documenter can also automatically deploy documentation to gh-pages.
+	# See "Hosting Documentation" and deploydocs() in the Documenter manual
+	# for more information.
+	deploydocs(
+		repo = "github.com/scottrsm/Wordle.jl.git"
+	)
